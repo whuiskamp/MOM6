a28913133175e407549c9e09b7a92d103e85ef29
Willem Huiskamp <willemhuiskamp88@gmail.com>
2022-04-29 12:14:39 +0200

    missing comma

--- src/parameterizations/vertical/MOM_diabatic_aux.F90
+++ src/parameterizations/vertical/MOM_diabatic_aux.F90
@@ -1057,7 +1057,7 @@ subroutine applyBoundaryFluxesInOut(CS, G, GV, US, dt, fluxes, optics, nsw, h, t
     netheat_rate, &  ! netheat but for dt=1 [degC H T-1 ~> degC m s-1 or degC kg m-2 s-1]
     netsalt_rate, &  ! netsalt but for dt=1 (e.g. returns a rate)
                      ! [ppt H T-1 ~> ppt m s-1 or ppt kg m-2 s-1]
-    netMassInOut_rate! netmassinout but for dt=1 [H T-1 ~> m s-1 or kg m-2 s-1]
+    netMassInOut_rate,! netmassinout but for dt=1 [H T-1 ~> m s-1 or kg m-2 s-1]
     basal_thk,    &  ! basal mass flux from PIK_basal routines
     basal_heat,   &  ! basal heat flux from PIK_basal routines
     basal_depth   &  ! depth of insertion for basal mass and heat fluxes
