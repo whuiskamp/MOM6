6267c6af9ec774153614ad1b44d846cfdbcfb43f
Willem Huiskamp <willemhuiskamp88@gmail.com>
2022-07-20 12:14:01 +0200

    Urgh typo....

--- src/parameterizations/vertical/MOM_diabatic_aux.F90
+++ src/parameterizations/vertical/MOM_diabatic_aux.F90
@@ -1420,7 +1420,7 @@ subroutine applyBoundaryFluxesInOut(CS, G, GV, US, dt, fluxes, optics, nsw, h, t
                 hOld     = h2d(i,k)                  ! We need the initial thickness
                 h2d(i,k) = h2d(i,k) + basal_thk(i) ! Update thickness with basal melt
                 Ithickness  = 1.0/h2d(i,k)           ! Inverse new thickness
-                write(0,*) 'At i,j= ', i,','j,', Old h= ',hOld,' at level k=',k,' is ' //&
+                write(0,*) 'At i,j= ', i,',',j,', Old h= ',hOld,' at level k=',k,' is ' //&
                             'increased by ',basal_thk(i),'m, resulting in a new thickness ' //&
                             'of ', h2d(i,k),'m.'
                 !!!  Update temp. due to mass change !!!
